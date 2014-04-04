4beb001aaf17084c77138d2d8059172f51dfc60a
Daniel Conroy <daniel.conroy@my.wheaton.edu>
2014-04-03 22:07:05 -0500

    added an additional menu option and added a new view for adding an event, though there is currently no functionality.

--- gen/com/wheaton/cs335/androidtimeline/R.java
+++ gen/com/wheaton/cs335/androidtimeline/R.java
@@ -25,21 +25,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int AddEvent=0x7f080002;
         public static final int action_settings=0x7f080001;
         public static final int container=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_add_event=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_add_event=0x7f030002;
+        public static final int fragment_main=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int add_event=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int editCheckBox=0x7f050007;
+        public static final int eventDetails=0x7f05000b;
+        public static final int eventTitle=0x7f050006;
+        public static final int firstDate=0x7f050008;
         public static final int greeting=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int newEventTitle=0x7f050005;
+        public static final int secondDate=0x7f050009;
+        public static final int tempCategory=0x7f05000a;
+        public static final int title_activity_add_event=0x7f050004;
     }
     public static final class style {
         /** 
