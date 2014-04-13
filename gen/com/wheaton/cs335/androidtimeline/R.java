d4d5b8c9ef8791cb1b90b85903a4cdbe38cd2deb
Josh Wright <joshuawright11@gmail.com>
2014-04-13 18:25:37 -0500

    Add list view. Merging before populating it.

--- gen/com/wheaton/cs335/androidtimeline/R.java
+++ gen/com/wheaton/cs335/androidtimeline/R.java
@@ -25,8 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080003;
         public static final int container=0x7f080000;
+        public static final int textView1=0x7f080001;
+        public static final int timelineListView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,6 +42,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int greeting=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int timelineListviewCaption=0x7f050004;
     }
     public static final class style {
         /** 
