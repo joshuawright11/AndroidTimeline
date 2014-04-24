bd081c6db3d34222b0776d7e160c440fd5479443
Daniel Conroy <daniel.conroy@my.wheaton.edu>
2014-04-23 19:44:19 -0500

    added Documentation to AddCategory and AddEvent.

--- src/com/wheaton/cs335/androidtimeline/AddEvent.java
+++ src/com/wheaton/cs335/androidtimeline/AddEvent.java
@@ -5,60 +5,67 @@ import model.Duration;
 import model.Category;
 import model.TLEvent;
 import model.Timeline;
-
-import java.io.IOException;
 import java.sql.Date;
-import java.util.Arrays;
 import java.util.Iterator;
-
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.os.Build;
-
 import java.util.ArrayList;
-
-import org.json.simple.parser.ParseException;
-
-import storage.DBHelperAPI;
-import storage.phpPushHelper;
 
 public class AddEvent extends Activity {
 	
+	/**
+	 * This a variable to stand as a reference to this activity.
+	 */
 	private static Activity thisActivity;
 	
-	boolean checkBox;
+	/**
+	 * This is a boolean check for the state of the checkBox.
+	 * True = Duration Event, False = Atomic Event
+	 */
+	private boolean checkBox;
 	
-	static ArrayList<Category> categories;
+	/**
+	 * This is the array of timelines from MainActivity.
+	 */
+	private static ArrayList<Timeline> timelines;
 	
-	static ArrayList<Timeline> timelines;
+	/**
+	 * This is the ArrayAdapter for the tSpinner.
+	 */
+	private static ArrayAdapter<String> timeSelector;
 	
-	static DBHelperAPI database;
+	/**
+	 * This is the Spinner for selecting a timeline.
+	 */
+	private static Spinner tSpinner;
 	
-	static ArrayAdapter<String> timeSelector;
+	/**
+	 * This is the ArrayAdapter for the cSpinner.
+	 */
+	private static ArrayAdapter<String> catSelector;
 	
-	static Spinner tSpinner;
-	
-	static ArrayAdapter<String> catSelector;
-	
-	static Spinner cSpinner;
+	/**
+	 * This is the SPinner for selecting a Category.
+	 */
+	private static Spinner cSpinner;
 
+	@SuppressWarnings("unchecked")
 	@Override
+	/**
+	 * This is the method called upon creation of this Activity.
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_event);
@@ -75,6 +82,14 @@ public class AddEvent extends Activity {
 		timelines = (ArrayList<Timeline>) getIntent().getSerializableExtra("timelines");
 	}
 	
+	/**
+	 * This method is called when a button is clicked in this Activity. In particular, this means
+	 * that when the OK button is selected, an event is constructed, atomic or duration based on the
+	 * state of the "checkBox", the category being retrieved from the Category Spinner, and the event
+	 * is added to the Timeline. Subsequently, the timelines are saved to the database and finish() is called.
+	 * 
+	 * @param view
+	 */
 	public void okClick(View view) {
 		Timeline t = timelines.get(tSpinner.getSelectedItemPosition());
 		String title = ((EditText) findViewById(R.id.EventTitle)).getText().toString();
@@ -105,6 +120,11 @@ public class AddEvent extends Activity {
 		finish();
 	}
 	
+	/**
+	 * This is the method called when the CheckBox is checked, changing the boolean "checkbox"
+	 * and making visible or invisible the second date and second date picker.
+	 * @param view
+	 */
 	public void onCheckboxClicked(View view) {
 		TextView text = (TextView) findViewById(R.id.secondDate);
 		DatePicker picker = (DatePicker) findViewById(R.id.datePicker2);
@@ -119,6 +139,9 @@ public class AddEvent extends Activity {
 	}
 
 	@Override
+	/**
+	 * This method populates the menu.
+	 */
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -130,11 +153,17 @@ public class AddEvent extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
-
+		
+		/**
+		 * Constructor
+		 */
 		public PlaceholderFragment() {
 		}
 
 		@Override
+		/**
+		 * This method adds the Fragment to the Activity.
+		 */
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_add_event,
@@ -159,6 +188,10 @@ public class AddEvent extends Activity {
 			tSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 				@Override
+				/**
+				 * This is a listener for when a new Timeline is selected. This populates
+				 * the category spinner with the updated categories.
+				 */
 				public void onItemSelected(AdapterView<?> parent, View view,
 						int position, long id) {
 					Timeline t = timelines.get(position);
@@ -167,18 +200,11 @@ public class AddEvent extends Activity {
 		        		catNames.add(it.next().getName());
 		        	}
 		        	catSelector = new ArrayAdapter<String> (thisActivity, android.R.layout.simple_list_item_1, catNames);
-					
 				}
-
 				@Override
-				public void onNothingSelected(AdapterView<?> parent) {
-					// TODO Auto-generated method stub
-					
-				}
+				public void onNothingSelected(AdapterView<?> parent) {}
 		    });
-			
 			return rootView;
 		}
 	}
-
 }
